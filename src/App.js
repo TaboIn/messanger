4e124567b6dc9830fcf99fdcff5fd643834b8498
TaboIn <yourlasdream@gmail.com>
2022-12-29 13:11:16 +1000

    dsfsdfsd

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import './App.css'
 import Header from './components/header/Header'
 
 function App() {
 	return (
-		<div className='App'>
+		<div className='w-full h-full'>
 			<Header />
 		</div>
 	)
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Header = () => {
 	return (
-		<div className='w-full h-[32px] bg-green'>
-			<div className=''></div>
+		<div className='w-full h-[32px] bg-slate-700'>
+			<div className='w-full bg-black'>
+				<p className='py-4'>test</p>
+			</div>
 		</div>
 	)
 }
